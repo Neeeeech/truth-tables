0311fb68ffa08564cc27d9a2de1d6f4463bbb7a0
Neeeeech <leungnicholas121@gmail.com>
2021-02-20 22:10:00 +0800

    Now supports any no. of vars, user-defined. Fixed parsing for '(word'.

--- main.py
+++ main.py
@@ -1,60 +1,14 @@
-pretty = {True: 'True ', False: 'False'}
+pretty = {True: ' True   ', False: ' False  '}
 letters = {'P': 0, 'Q': 1, 'R': 2}
+unopDict = {'T': lambda a: True,
+            'F': lambda a: False}
 
-T = lambda p, q, r: True
-F = lambda p, q, r: False
-P = lambda p, q, r: p
-Q = lambda p, q, r: q
-R = lambda p, q, r: r
-
-NOT = lambda f1: lambda p, q, r: not f1(p, q, r)
-OR = lambda f1, f2: lambda p, q, r: f1(p, q, r) or f2(p, q, r)
-AND = lambda f1, f2: lambda p, q, r: f1(p, q, r) and f2(p, q, r)
-IMPLIES = lambda f1, f2: lambda p, q, r: not (f1(p, q, r) and not f2(p, q, r))
+NOT = lambda f1: lambda a: not f1(a)
+OR = lambda f1, f2: lambda a: f1(a) or f2(a)
+AND = lambda f1, f2: lambda a: f1(a) and f2(a)
+IMPLIES = lambda f1, f2: lambda a: not (f1(a) and not f2(a))
 CIMPLIES = lambda f1, f2: IMPLIES(f2, f1)
-XOR = lambda f1, f2: lambda p, q, r: (f1(p, q, r) and f2(p, q, r)) or (not f1(p, q, r) and not f2(p, q, r))
-
-def iterTable(func, arr):
-    has, ind = [], [iter0, iter1, iter2, iter3]
-    for i in ['P', 'Q', 'R']:
-        if i in arr:
-            has.append(i)
-    ind[len(has)](func, has)
-    print('\n')
-
-
-def iter0(func, vars):
-    print('RESULT')
-    print(func(True, True, True))
-
-
-def iter1(func, vars):
-    print(f'   {vars[0]}   :  RESULT')
-    inp = [True, True, True]
-    inp[letters[vars[0]]] = False
-    print(f' True  :  {func(True, True, True)}')
-    print(f' False :  {func(inp[0], inp[1], inp[2])}')
-
-
-def iter2(func, vars):
-    print(f'   {vars[0]}       {vars[1]}    :  RESULT')
-    inp = [True, True, True]
-    for i in range(2):
-        inp[letters[vars[1]]] = True
-        for j in range(2):
-            print(f' {pretty[inp[letters[vars[0]]]]}   {pretty[inp[letters[vars[1]]]]}  '
-                  f':  {func(inp[0], inp[1], inp[2])}')
-            inp[letters[vars[1]]] = False
-        inp[letters[vars[0]]] = False
-
-
-def iter3(func, vars):
-    print('   P       Q       R    :  RESULT')
-    for p in [True, False]:
-        for q in [True, False]:
-            for r in [True, False]:
-                print(f' {pretty[p]}   {pretty[q]}   {pretty[r]}  :  {func(p, q, r)}')
-
+XOR = lambda f1, f2: lambda a: (f1(a) and f2(a)) or (not f1(a) and not f2(a))
 
 wordKeys = {'and': ['&'],
             'or': ['V'],
@@ -66,30 +20,57 @@ wordKeys = {'and': ['&'],
             'nand': ['!', '&'],
             'xor': ['X']}
 
-unopDict = {'P': P,
-            'Q': Q,
-            'R': R,
-            'T': T,
-            'F': F}
-
 biopDict = {'V': OR,
             '&': AND,
             'I': IMPLIES,
             'C': CIMPLIES,
             'X': XOR}
 
+def unopInit(vars):
+    global unopDict
+    unopDict = {'T': lambda a: True, 'F': lambda a: False}
+    for i in range(len(vars)):
+        if len(vars[i]) != 1:
+            print('Has to be one letter only')
+            quit()
+        elif vars[i].upper() in ['V', '&', '(', ')', '!', 'X']:
+            print('Cannot be an operation')
+            quit()
+        elif vars[i].upper() in ['T', 'F']:
+            print("'T' and 'F' already defined")
+            quit()
+        unopDict[vars[i].upper()] = lambda a, b=i: a[b]
+
+def head(vars):
+    return ''.join([f'   {var}    ' for var in vars]) + ':  RESULT' if vars else 'RESULT'
+
+def body(func, vars, i):
+    if not vars: return f' {func(None)}'
+    varVal = [(i // (2**j)) % 2 == 0 for j in range(len(vars)-1, -1, -1)]
+    inputs = [varVal[vars.index(var)] if var in vars else True for var in list(unopDict.keys())[2:]]
+    return ''.join([pretty[inp] for inp in varVal]) + f':   {func(inputs)}'
+
+def iterTable(func, opList):
+    vars = [var for var in list(unopDict.keys())[2:] if var in opList]
+    print(head(vars))
+    for i in range(2**len(vars)):
+        print(body(func, vars, i))
+    print()
+
 
 def parse(inp):
-    if inp == 'quit':
+    if inp.lower() == 'quit':
         quit()
     arr, inp, j = [], inp.split(' '), 0
     while ' ' in inp:
         inp.remove(' ')
     while j < len(inp):
-        input = inp[j]
-        if input.lower() in wordKeys.keys():
-            arr = arr + wordKeys[input.lower()]
-        elif input.lower() == 'if':
+        word = inp[j]
+        if word.lower() in wordKeys.keys():
+            arr = arr + wordKeys[word.lower()]
+        elif word[0] == '(' and word[1:].lower() in wordKeys.keys():
+            arr = arr + ['('] + wordKeys[word[1:].lower()]
+        elif word.lower() == 'if':
             try:
                 if inp[j + 1].lower() == 'and' and inp[j + 2].lower() == 'only' and inp[j + 3].lower() == 'if':
                     arr.append('X')
@@ -102,13 +83,13 @@ def parse(inp):
                 quit()
         else:
             i = 0
-            while i < len(input):
-                if input[i].upper() in ['V', '&', 'T', 'F', 'P', 'Q', 'R', '(', ')', '!', 'X']:
-                    arr.append(input[i].upper())
-                elif input[i] == '<':
-                    if input[i + 1] == '-':
+            while i < len(word):
+                if word[i].upper() in ['V', '&', '(', ')', '!', 'X'] or word[i].upper() in unopDict.keys():
+                    arr.append(word[i].upper())
+                elif word[i] == '<':
+                    if word[i + 1] == '-':
                         try:
-                            if input[i + 2] == '>':
+                            if word[i + 2] == '>':
                                 arr.append('X')
                                 i += 2
                             else:
@@ -118,23 +99,22 @@ def parse(inp):
                             arr.append('C')
                             i += 1
                     else:
-                        print(f'Unrecognised character {input[i + 1]} after <, please try <- or <->')
+                        print(f'Unrecognised character {word[i + 1]} after <, please try <- or <->')
                         quit()
-                elif input[i] == '-':
-                    if input[i + 1] == '>':
+                elif word[i] == '-':
+                    if word[i + 1] == '>':
                         arr.append('I')
                         i += 1
                     else:
-                        print(f'Unrecognised character {input[i + 1]} after -, please try ->')
+                        print(f'Unrecognised character {word[i + 1]} after -, please try ->')
                         quit()
-                elif input[i] == ' ':
+                elif word[i] == ' ':
                     pass
                 else:
-                    print(f'Unrecognised character {input[i]}')
+                    print(f'Unrecognised character {word[i]}')
                     quit()
                 i += 1
         j += 1
-    print()
     return arr
 
 
@@ -203,7 +183,8 @@ def process(arr, biop2=False):
                 quit()
 
 
-print('\n\nType in a logic statement, with variables p,q,r and the following operations\n'
+print("\n\nWhen prompted 'vars?' please define one-letter variable names separated by spaces\n"
+      "When prompted '?' type in a logic statement, with defined variables and following operations:\n"
       'Accepted Inputs - meaning\n'
       'T, true       - truth / tautology\n'
       'F, false      - contradiction\n'
@@ -216,9 +197,16 @@ print('\n\nType in a logic statement, with variables p,q,r and the following ope
       '<->, xor      - if and only if\n'
       '!V, nor       - nor\n'
       '!&, nand      - not and\n'
+      'redef         - redefine variable names\n'
       'quit          - quit the program\n')
 
-while True:
-    a = parse(input('? '))
-    b = process(a)
-    iterTable(b, a)
+if __name__ == '__main__':
+    unopInit(input('vars? ').split())
+    while True:
+        a = input('? ')
+        if a == 'redef':
+            unopInit(input('vars? ').split())
+            continue
+        opList = parse(a)
+        func = process(opList)
+        iterTable(func, opList)
